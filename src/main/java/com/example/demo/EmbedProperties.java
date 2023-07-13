7cc3e65671e36cba9235ed810c2b4a8a6401b080
Nithya <nithya.gopal@syncfusion.com>
2023-07-13 08:46:21 +0530

    319743-Configure-embedConfigJson-instead-of-EmbedPropertie-in-Spring-Boot-Sample

--- src/main/java/com/example/demo/EmbedProperties.java
+++ src/main/java/com/example/demo/EmbedProperties.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class EmbedProperties {
     
-public static  String email ="nithya.gopal@syncfusion.com";//UserEmail of the Admin in your Bold BI, which would be used to get the dashboards list
-public static String embedSecret ="r5eT5bKa1g0LcU7aXbu1LAUMf2lal85i";//	Get your EmbedSecret key from the Embed tab by enabling the Enable embed authentication in the Administration page(https://help.boldbi.com/embedded-bi/site-administration/embed-settings/)
+public static  String email ="";//UserEmail of the Admin in your Bold BI, which would be used to get the dashboards list
+public static String embedSecret ="";//	Get your EmbedSecret key from the Embed tab by enabling the Enable embed authentication in the Administration page(https://help.boldbi.com/embedded-bi/site-administration/embed-settings/)
 }
