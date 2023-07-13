144b7bf5e1527d518ddbac9aaa4d38a93b35503c
dharshinivivek <dharshini.sf4021@gmail.com>
2023-07-13 19:58:23 +0530

    Added files to embedconfig.json

--- src/main/java/com/example/demo/EmbedProperties.java
+++ src/main/java/com/example/demo/EmbedProperties.java
@@ -1,7 +1,92 @@
-package com.example.demo;
+package com.example.demo; 
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class EmbedProperties {
-    
-public static  String email ="";//UserEmail of the Admin in your Bold BI, which would be used to get the dashboards list
-public static String embedSecret ="";//	Get your EmbedSecret key from the Embed tab by enabling the Enable embed authentication in the Administration page(https://help.boldbi.com/embedded-bi/site-administration/embed-settings/)
+    @JsonProperty("DashboardId")
+    private String dashboardId;
+
+    @JsonProperty("ServerUrl")
+    private String serverUrl;
+
+    @JsonProperty("UserEmail")
+    private static String userEmail;
+
+    @JsonProperty("EmbedSecret")
+    private static String embedSecret;
+
+    @JsonProperty("EmbedType")
+    private String embedType;
+
+    @JsonProperty("Environment")
+    private String environment;
+
+    @JsonProperty("ExpirationTime")
+    private String expirationTime;
+
+    @JsonProperty("SiteIdentifier")
+    private String siteIdentifier;
+
+    public String getDashboardId() {
+        return dashboardId;
+    }
+
+    public void setDashboardId(String dashboardId) {
+        this.dashboardId = dashboardId;
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public static String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        EmbedProperties.userEmail = userEmail;
+    }
+
+    public static String getEmbedSecret() {
+        return embedSecret;
+    }
+
+    public void setEmbedSecret(String embedSecret) {
+        EmbedProperties.embedSecret = embedSecret;
+    }
+
+    public String getEmbedType() {
+        return embedType;
+    }
+
+    public void setEmbedType(String embedType) {
+        this.embedType = embedType;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    public String getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(String expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+
+    public String getSiteIdentifier() {
+        return siteIdentifier;
+    }
+
+    public void setSiteIdentifier(String siteIdentifier) {
+        this.siteIdentifier = siteIdentifier;
+    }
 }
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,24 +1,42 @@
 package com.example.demo; 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+import com.google.gson.Gson;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/")
 public class HomeController {
+   @GetMapping(value = "embedConfig", produces = MediaType.APPLICATION_JSON_VALUE)
+public EmbedProperties getEmbedConfig() throws IOException {
+    ClassPathResource resource = new ClassPathResource("embedConfig.json");
+    byte[] jsonBytes = StreamUtils.copyToByteArray(resource.getInputStream());
+    String jsonContent = new String(jsonBytes, StandardCharsets.UTF_8);
 
+    Gson gson = new Gson();
+    EmbedProperties embedProperties = gson.fromJson(jsonContent, EmbedProperties.class);
+    
+    return embedProperties;
+}
     @PostMapping("GetDetails")
     public String getDetails(@RequestBody EmbedClass embedQuerString) throws Exception{
         String embedQuery = embedQuerString.getEmbedQuerString();
-        embedQuery+= "&embed_user_email=" + EmbedProperties.email;
+        embedQuery+= "&embed_user_email=" + EmbedProperties.getUserEmail();
         String embedDetailsUrl = "/embed/authorize?" + embedQuery + "&embed_signature=" + GetSignatureUrl(embedQuery);
         RestTemplate restTemplate = new RestTemplate();
         DefaultUriBuilderFactory defaultUriBuilderFactory = new DefaultUriBuilderFactory();
@@ -31,7 +49,7 @@ public class HomeController {
     public String GetSignatureUrl(String queryString) throws Exception {
         if (queryString != null){
         Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
-        SecretKeySpec secret_key = new SecretKeySpec(EmbedProperties.embedSecret.getBytes("UTF-8"), "HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(EmbedProperties.getEmbedSecret().getBytes("UTF-8"), "HmacSHA256");
         sha256_HMAC.init(secret_key);
         	return new String(Base64.getEncoder().encode(sha256_HMAC.doFinal(queryString.getBytes("UTF-8"))));
             }
